53dfd96a49ee0079c6c1e6cd88c63d3f6b61163b
Rodolfo <rodolfo@princiweb.com.br>
2015-03-07 15:52:27 -0300

    base backend e frontend

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,22 @@ var gulp        = require('gulp')
   , browserSync = require('browser-sync')
   , reload      = browserSync.reload;
 
+gulp.task('nodemon', function(cb) {
+  return plugins.nodemon({
+    script: './build/backend/app.js'
+  }).on('start', function () {
+    cb();
+  });
+});
+
+gulp.task('backend', function() {
+  utils.log(utils.colors.black.bgGreen('Backend'));
+
+  gulp.src(paths.src.backend)
+    .pipe(plugins.changed(paths.build.backend))
+    .pipe(gulp.dest(paths.build.backend));
+});
+
 gulp.task('vendorjs', function() {
   utils.log(utils.colors.black.bgGreen('Vendor JavaScript'));
 
@@ -14,10 +30,10 @@ gulp.task('vendorjs', function() {
     .pipe(reload({stream: true}));
 });
 
-gulp.task('app', function() {
-  utils.log(utils.colors.black.bgGreen('App'));
+gulp.task('appjs', function() {
+  utils.log(utils.colors.black.bgGreen('App - JS'));
 
-  gulp.src(paths.src.app)
+  gulp.src(paths.src.appjs)
     .pipe(plugins.changed(paths.build.app))
     .pipe(plugins.ngAnnotate({
       add: true,
@@ -27,4 +43,25 @@ gulp.task('app', function() {
     .pipe(reload({stream: true}));
 });
 
-gulp.task('default', ['vendorjs', 'app', 'b-s']);
+gulp.task('app', function() {
+  utils.log(utils.colors.black.bgGreen('App'));
+
+  gulp.src(paths.src.app)
+    .pipe(plugins.changed(paths.build.app))
+    .pipe(gulp.dest(paths.build.app))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('b-s', ['nodemon'], function() {
+  browserSync.init(null, {
+    files: ['./build/frontend/**/*'],
+    port: 3000,
+    open: false
+  });
+
+  gulp.watch(paths.src.vendorjs, ['vendorjs']);
+  gulp.watch(paths.src.app, ['app']);
+  gulp.watch(paths.src.appjs, ['appjs']);
+});
+
+gulp.task('default', ['backend', 'vendorjs', 'app', 'appjs', 'b-s']);
