0a3c2bbdfe56f5fe105a1b61be894a2a3ba35afb
Victor Miguez <victorcineis@gmail.com>
2015-03-08 10:26:32 -0300

    junção do front-end

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ var path      = require("path");
 var mongoose  = require('mongoose');
 var bodyParser = require('body-parser');
 
-mongoose.connect('');
+mongoose.connect('mongodb://miguez:live3735@ds053251.mongolab.com:53251/miguez');
 
 var assetsPath = path.join(__dirname, '..', 'frontend');
 
@@ -16,10 +16,10 @@ app.use(bodyParser.urlencoded({
 
 var api = {};
 
-api.demanda = require('./api/demanda');
+// api.demanda = require('./api/demanda');
 
-app.post('/api/demanda', api.demanda.inserir);
-app.get('/api/demandas', api.demanda.obterTodas);
+// app.post('/api/demanda', api.demanda.inserir);
+// app.get('/api/demandas', api.demanda.obterTodas);
 
 app.get('*',function(req,res){
   res.sendFile(assetsPath + "/");
